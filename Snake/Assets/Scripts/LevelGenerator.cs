ad806f9900191116c4bf357855418d0a26d34600
Yodo780 <yodo780@gmail.com>
2022-10-14 11:04:08 +0500

    Добавил еды

--- Snake/Assets/Scripts/LevelGenerator.cs
+++ Snake/Assets/Scripts/LevelGenerator.cs
@@ -10,15 +10,19 @@ public class LevelGenerator : MonoBehaviour
     void Start()
     {
         GenerateLevel(1);
+        GenerateLevel(2);
+        GenerateLevel(3);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(0, 0, -0.05f));
+        transform.Translate(new Vector3(0, 0, -0.01f));
     }
 
-    public void GenerateLevel(int difficulty){
-        Instantiate(boosterPrefab, new Vector3(Random.Range(0, 5) * 2 - 4, 0, /*currentRow*/ 10), Quaternion.identity, gameObject.transform);
+    public void GenerateLevel(int line)
+    {
+        // создаем Food рандомно в одном из 5 полей, на растоянии от центра в 10 единиц, и вкладываем его в LevelConteiner
+        Instantiate(boosterPrefab, new Vector3(Random.Range(0, 5) * 2 - 4, 0, /*currentRow*/ 10 + line * 2), Quaternion.identity, gameObject.transform);
     }
 }
